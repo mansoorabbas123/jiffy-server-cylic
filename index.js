31bcc6811fefc3ac1c8c600f20db95f88f4e9544
mansoorabbas123 <mansoor.abbas651@gmail.com>
2024-02-06 15:48:28 -0500

    test dummy user for db testing

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
-const sequelize = require('./models');
-app.all('/', (req, res) => {
+const db = require('./models');
+
+app.all('/', async(req, res) => {
     console.log("Just got a request!")
+    const user = await db.User.findAll();
+    console.log("user",user)
     res.send('Yo!')
 })
 app.listen(process.env.PORT || 3000)
--- models/index.js
+++ models/index.js
@@ -9,54 +9,55 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
-// if (config.use_env_variable) {
-//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
-// } else {
-//   sequelize = new Sequelize(config.database, config.username, config.password, config);
-// }
-// fs
-//   .readdirSync(__dirname)
-//   .filter(file => {
-//     return (
-//       file.indexOf('.') !== 0 &&
-//       file !== basename &&
-//       file.slice(-3) === '.js' &&
-//       file.indexOf('.test.js') === -1
-//     );
-//   })
-//   .forEach(file => {
-//     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-//     db[model.name] = model;
-//   });
-
-// Object.keys(db).forEach(modelName => {
-//   if (db[modelName].associate) {
-//     db[modelName].associate(db);
-//   }
-// });
-
-// connect to sql db
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    dialect: 'postgres',
-    port: process.env.DB_Port,
-    host: process.env.DB_HOST,
-    logging: false,
-    models: [__dirname],
-    dialectOptions: {
-      ssl: {
-        require: true
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+      dialect: 'postgres',
+      port: process.env.DB_Port,
+      host: process.env.DB_HOST,
+      logging: false,
+      models: [__dirname],
+      dialectOptions: {
+        ssl: {
+          require: true
+        }
       }
-    }
-  },
-);
+    });
+}
+
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    return (
+      file.indexOf('.') !== 0 &&
+      file !== basename &&
+      file.slice(-3) === '.js' &&
+      file.indexOf('.test.js') === -1
+    );
+  })
+  .forEach(file => {
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
 
 async function testDB(){
 try {
-  await sequelize.authenticate();
+  await db.sequelize.authenticate();
   console.log('Connection has been established successfully.');
 } catch (error) {
   console.error('Unable to connect to the database:', error);
@@ -64,4 +65,4 @@ try {
 }
 testDB();
 
-module.exports = sequelize;
+module.exports = db;
